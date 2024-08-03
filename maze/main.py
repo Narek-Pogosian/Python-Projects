df625397aa5b5dd562faa2a12f4f156a3e27f91d
Narek Pogosian <narekpogosian2@gmail.com>
2024-08-03 22:23:52 +0200

    update gitgnore and start expense tracker project

--- maze/main.py
+++ maze/main.py
@@ -31,18 +31,41 @@ class Maze:
         self.start: MazeLocation = start
         self.goal: MazeLocation = goal
 
-        self._grid: List[List[Cell]] = [
-            [Cell.PATH for _ in range(columns)] for _ in range(rows)
-        ]
+        self._grid: List[List[Cell]] = [[Cell.PATH for _ in range(columns)] for _ in range(rows)]
         self._randomly_fill(rows, columns, sparseness)
         self._grid[start.row][start.column] = Cell.START
         self._grid[goal.row][goal.column] = Cell.GOAL
 
     def _randomly_fill(self, rows: int, columns: int, sparseness: float):
+        """Randomly fill blocks in maze, amount based on sparseness (0 - 1)"""
         for row in range(rows):
             for column in range(columns):
                 if random.uniform(0, 1.0) < sparseness:
                     self._grid[row][column] = Cell.BLOCKED
+
+    def goal_test(self, ml: MazeLocation):
+        """Check if current location is at the goal"""
+        return ml == self.goal
+    
+    def find_next_possible_locations(self, ml: MazeLocation) -> List[MazeLocation]:
+        """Return wich locations are available out of the top, right, bottom, left locations."""
+        if self._grid[ml.row][ml.column] == Cell.BLOCKED:
+            return
+        
+        locations: List[MazeLocation] = []
+        if ml.row + 1 < self._rows and self._grid[ml.row + 1][ml.column] != Cell.BLOCKED:
+            locations.append(MazeLocation(ml.row + 1, ml.column))
+
+        if ml.row - 1 >= 0 and self._grid[ml.row - 1][ml.column] != Cell.BLOCKED:
+            locations.append(MazeLocation(ml.row - 1, ml.column))
+
+        if ml.column + 1 < self._columns and self._grid[ml.row][ml.column + 1] != Cell.BLOCKED:
+            locations.append(MazeLocation(ml.row, ml.column + 1))
+
+        if ml.column - 1 >= 0 and self._grid[ml.row][ml.column - 1] != Cell.BLOCKED:
+            locations.append(MazeLocation(ml.row, ml.column - 1))
+            
+        return locations
 
     def __str__(self) -> str:
         output = ""
@@ -51,9 +74,17 @@ class Maze:
         return output
 
 
+def find_solution(maze: Maze):
+    """
+    Use DFS to find solution, go through each path as far as 
+    possible before backtrack.
+    """
+    print(maze.find_next_possible_locations(MazeLocation(2,3)))
+
+
 def main():
     maze = Maze()
-    print(maze)
+    find_solution(maze)
 
 
 if __name__ == "__main__":
